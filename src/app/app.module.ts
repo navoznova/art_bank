89d7f2fbecc250a9850995d69bc69ff64a6de90b
Anna Navoznova <anna.navoznova@gmail.com>
2022-10-06 13:32:56 +0800

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonsTableComponent } from './persons-table/persons-table.component';
+import { DeleteButtonComponent } from './delete-button/delete-button.component';
+import { AddButtonComponent } from './add-button/add-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonsTableComponent,
+    DeleteButtonComponent,
+    AddButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/persons-table/persons-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-persons-table',
+  templateUrl: './persons-table.component.html',
+  styleUrls: ['./persons-table.component.css']
+})
+export class PersonsTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
